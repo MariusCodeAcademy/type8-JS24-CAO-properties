e111f04ec6c51269b202e7c2ffe18027178e9be6
Marius <marius@example.com>
2022-03-29 08:46:31 +0300

    get posts

--- js/posts.js
+++ js/posts.js
@@ -1,1 +1,11 @@
 const baseUrl = 'https://one-more-mca.herokuapp.com/api/posts';
+
+// parsisiunciam visus posts ir isconsolinam
+async function getPosts() {
+  const resp = await fetch(baseUrl);
+  const dataInJs = await resp.json();
+  console.log('dataInJs ===', dataInJs);
+}
+getPosts();
+
+// generuojam korteles kaip ir su properties
